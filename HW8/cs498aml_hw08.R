9f89bf0ee495a9b8a30bee9a7b0811ad365811fd
Kirk Kittell <kirk.kittell@gmail.com>
2018-04-16 08:06:34 -0500

    Update cs498aml_hw08.R

--- HW8/cs498aml_hw08.R
+++ HW8/cs498aml_hw08.R
@@ -1,13 +1,34 @@
 # switch for working on home vs work computer...
-mac <- FALSE
+mac <- TRUE
 
-stop <- TRUE
+stop <- FALSE
 stopAndGo <- function() {
   # Stop time to grab plots and drawings during loops, but only if
   # the global variable 'stop' is TRUE
   if (stop) {
     invisible(readline(prompt="Press [enter] to continue"))
   }
+}
+
+image.vector.to.matrix <- function(image.vector) {
+  
+}
+
+image.matrix.to.vector <- function(image.matrix) {
+  
+}
+
+hw8.col <- function(image.side, i, j) {
+  c <- image.side * (noise.df[(2*i)-1, j] - 1) + noise.df[2*i, j]
+}
+
+load.supplement <- function(supplement.file) {
+  # Shortcut for loading the various supplemental files
+  # How to return two objects: https://stackoverflow.com/a/15140507/752784
+  supplement.df <- read.csv(supplement.file, header=TRUE)
+  rownames(supplement.df) <- supplement.df[,1]
+  supplement.df <- supplement.df[,-1]
+  supplement.df <- supplement.df + 1    # Account for files being zero-indexed
 }
 
 image.bin.draw <- function(image.vector) {
@@ -23,17 +44,21 @@ image.bin.draw <- function(image.vector) {
 }
 
 image.bin.draw.all <- function(image.all) {
-for (i in 1:nrow(image.all)) {
-  image.bin.draw(unlist(image.all[i,], use.names=FALSE))
-}
+  for (i in 1:nrow(image.all)) {
+    image.bin.draw(unlist(image.all[i,], use.names=FALSE))
+  }
 }
 
 if (mac) {
   setwd('/Users/kirkkittell/Dropbox/CS498AML/HW8')
   noise.file <- 'SupplementaryAndSampleData/NoiseCoordinates.csv'
+  update.file <- 'SupplementaryAndSampleData/UpdateOrderCoordinates.csv'
+  Q.file <- 'SupplementaryAndSampleData/InitialParametersModel.csv'
 } else {
   setwd('C:\\Users\\sd931e\\Documents\\kwk\\cs498aml\\HW8')
   noise.file <- 'SupplementaryAndSampleData\\NoiseCoordinates.csv'
+  update.file <- 'SupplementaryAndSampleData\\UpdateOrderCoordinates.csv'
+  Q.file <- 'SupplementaryAndSampleData\\InitialParametersModel.csv'
 }
 
 data.file <- 'mnist_train_20.csv'
@@ -50,10 +75,7 @@ mnist.df.bin[mnist.df.bin>=128] <- 1
 #image.bin.draw.all(mnist.df.bin)
 
 # Add noise to the dataset
-noise.df <- read.csv(noise.file, header=TRUE)
-rownames(noise.df) <- noise.df[,1]
-noise.df <- noise.df[,-1]
-noise.df <- noise.df + 1    # Account for noise.file being zero-indexed
+noise.df <- load.supplement(noise.file)
 mnist.df.noisy <- mnist.df.bin
 
 # Flip bits
@@ -61,7 +83,7 @@ image.side <- sqrt(ncol(mnist.df))
 for (i in 1:(nrow(noise.df)/2)) {
   for (j in 1:ncol(noise.df)) {
     r <- i
-    c <- image.side * (noise.df[(2*i)-1, j] - 1) + noise.df[2*i, j]
+    c <- hw8.col(image.side, i, j)
     mnist.df.noisy[r, c] <- -1 * mnist.df.noisy[r, c]
   }
 }
@@ -69,3 +91,7 @@ for (i in 1:(nrow(noise.df)/2)) {
 # Draw the noisy images
 image.bin.draw.all(mnist.df.noisy)
 
+# Denoise the images
+update.df <- load.supplement(update.file)
+Q.initial <- as.matrix(read.csv(Q.file, header=FALSE))
+Q <- list()
